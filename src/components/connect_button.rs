01c02849fd5974e29fe62917d2279d5c3d83e4e7
fc0101 <felix.crypto.0101@gmail.com>
2024-05-13 09:11:25 -0400

    fix: connect_wallet function

--- src/components/connect_button.rs
+++ src/components/connect_button.rs
@@ -1,17 +1,29 @@
 use yew::prelude::*;
 
-use gloo_console::log;
-use wasm_bindgen::JsValue;
+use wasm_bindgen_futures::spawn_local;
+use web3::transports::eip_1193::{Eip1193, Provider};
 
 #[function_component]
 pub fn ConnectButton() -> Html {
     let connect = {
-        Callback::from(|_|{
-            log!(JsValue::from("click 'Connect Wallet' button"));
+        Callback::from(move |_| {
+            spawn_local(async {
+                connect_wallet().await;
+            });
         })
     };
 
     html! {
         <div onclick={connect}>{ "Connect Wallet" }</div>
     }
+}
+
+async fn connect_wallet() {
+    let provider: Provider = Provider::default().unwrap().unwrap();
+
+    let transport = Eip1193::new(provider);
+
+    let web3 = web3::Web3::new(transport);
+
+    web3.eth().request_accounts().await.unwrap();
 }
